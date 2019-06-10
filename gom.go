3ecb0f9054263a81bf6ddc1ba43e8630eb73294d
5046312 <5046312@qq.com>
2019-06-10 14:21:43 +0800

    first commit

---
+++ gom.go
@@ -0,0 +1,43 @@
+package gom
+
+import (
+	"log"
+
+	"github.com/fsnotify/fsnotify"
+)
+
+func main() {
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer watcher.Close()
+
+	go func() {
+		for {
+			select {
+			case event, ok := <-watcher.Events:
+				if !ok {
+					return
+				}
+				log.Println("event:", event)
+				if event.Op&fsnotify.Write == fsnotify.Write {
+					log.Println("modified file:", event.Name)
+				}
+			case err, ok := <-watcher.Errors:
+				if !ok {
+					return
+				}
+				log.Println("error:", err)
+			}
+		}
+	}()
+
+	err = watcher.Add("./")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	done := make(chan bool)
+	<-done
+}
